6b12bf313e6c0b1c7d328ecad83442aa1f7ffae6
Ashritha <janagamashrithareddy@gmail.com>
2026-06-27 18:26:39 +0530

    updated parenthesis

--- Easy/20.py
+++ Easy/20.py
@@ -19,4 +19,25 @@ obj = Solution()
 s = "()[]{}"
 print(obj.isValid(s))
 
+##
+class Solution:
+    def isValid(self, s:str):
+        stack = []
+        brackets = {")":"(", "}": "{", "]":"["}
+
+        for char in s:
+            if char in brackets: #we are checking with keys
+                if stack and stack[-1] == brackets[char]:
+                    stack.pop() 
+                else:
+                    return False
+            else:
+                stack.append(char)
+        return len(stack) == 0
+    
+
+obj = Solution() 
+s = "()[]{}"
+print(obj.isValid(s))
+
 
