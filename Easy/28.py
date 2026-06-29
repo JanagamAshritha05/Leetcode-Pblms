c948f40f606325480281209560b83af85760ecb4
Ashritha <janagamashrithareddy@gmail.com>
2026-06-29 08:46:36 +0530

    updated first_index_occurance

--- Easy/28.py
+++ Easy/28.py
@@ -13,13 +13,20 @@ print(obj.strStr(haystack, needle))
 # 
 class Solution:
     def strStr(self, haystack, needle):
-        for i in range(len(haystack) - len(needle) + 1):
-            if haystack[i:i+len(needle)] == needle:
-                return i
-        return -1
+        for i in range(len(haystack)-len(needle) + 1):
+            match = True 
+            for j in range(len(needle)):
+                if haystack[i+j] != needle[j]:
+                    match = False 
+                    break 
+            if match:
+                return i 
+        return -1 
 
 obj = Solution() 
 haystack = "sadbutsad"
 needle = "sad"
 print(obj.strStr(haystack, needle))
+
+
 
